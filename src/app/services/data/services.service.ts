73f105df6c82f0fcde951b1337909dbeab4f40d4
Donovan Brown <Dess5000@gmail.com>
2018-09-17 13:55:54 -0400

    update portfolio

--- src/app/services/data/services.service.ts
+++ src/app/services/data/services.service.ts
@@ -113,7 +113,7 @@ export class DataService {
         img:'../../assets/img/employee.png', 
         description:'Developed a employee based CURD (Create, Read, Update, Delete) application where you can add, input, edit, and delete employment data. View employee data in a table format with page based navigation listing 10 employees per page storing data using Firebase developed with: HTML, CSS, JavaScript, TypeScript, Angular, NodeJS, Angular CLI, Font-Awesome, Bootstrap, and Firebase.', 
         subject:'Web', 
-        liveLink:false, 
+        liveLink:'https://dcbrown.github.io', 
         gitHubLink:'https://github.com/DCbrown/Angular-Firebase-CRUD'
       }
     ]  
